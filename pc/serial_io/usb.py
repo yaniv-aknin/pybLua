23a98ebd0629bfae0509216ab3e07189b5010f61
Yaniv Aknin <yaniv@aknin.name>
2009-08-29 01:09:19 +0300

    reduce default usb speed

--- pc/serial_io/usb.py
+++ pc/serial_io/usb.py
@@ -3,7 +3,7 @@ from base import SerialController
 from protocols.pblua import pbLuaConsoleProtocol
 
 class USBController(SerialController):
-    baudrate = 38400
+    baudrate = 19200
     defaultProtocolFactory = pbLuaConsoleProtocol
     def setState(self, state, *args, **kwargs):
         self.protocol.setState(state, *args, **kwargs)
