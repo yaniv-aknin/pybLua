cc921cc119dc2f12f39119b95bec12891bc78ff0
Yaniv Aknin <yaniv@aknin.name>
2009-08-28 12:53:42 +0300

    cleanups for protocol utils

--- pc/protocol_utils.py
+++ pc/protocol_utils.py
@@ -1,9 +1,8 @@
-from twisted.python import log
-
-class ProtocolStack:
+class ProtocolStack(object):
     # HACK: a crude protocol stack which allows a single transport to have several protocols
     #        this does not handle all sorts of cases, like setattr() on the protocol (it will just be set on the
     #        ProtocolStack instance and never be used) or calling connectionLost() upon self.pop()
+    __slots__ = ['push', 'pop', '_protocolStack', 'factory']
     def __init__(self, initialProtocol=None):
         self._protocolStack = [initialProtocol] if initialProtocol else []
     def __call__(self):
@@ -13,7 +12,8 @@ class ProtocolStack:
             raise AttributeError('tried gettting an attribute with no concrete protocol')
         return getattr(self._protocolStack[-1], attribute_name)
     def __repr__(self):
-        return '<%s using %r>' % (self._protocolStack[-1],)
+        return '<%s using %r>' % (self.__class__.__name__,
+                                  None if not self._protocolStack else self._protocolStack[-1])
     def push(self, protocol):
         self._protocolStack.append(protocol)
     def pop(self):
@@ -29,7 +29,6 @@ class TerminalBridgeProtocol(BaseBridgeProtocol):
         BaseBridgeProtocol.__init__(self, transport)
         self.keyHandlers = keyHandlers or {}
     def keystrokeReceived(self, keyID, modifier):
-        log.msg(repr(keyID))
         self.keyHandlers.get(keyID, self.transport.write)(keyID)
 class BridgeProtocol(BaseBridgeProtocol):
     def dataReceived(self, data):
