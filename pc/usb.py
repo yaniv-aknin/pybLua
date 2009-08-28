84f277f5e7d592a41ff9bf7f953b67469a624507
Yaniv Aknin <yaniv@aknin.name>
2009-08-28 19:05:04 +0300

    add some grace timeout upon startup of USBController if port is unavailable

--- pc/usb.py
+++ pc/usb.py
@@ -1,3 +1,4 @@
+import os
 from functools import partial
 
 from twisted.internet import reactor, protocol
@@ -6,19 +7,39 @@ from twisted.application.service import Service
 from twisted.internet.serialport import SerialPort
 from twisted.python import log
 
+from serial.serialutil import SerialException
+
 from pblua_serial import pbLuaSerialProtocol
 
+class UnableToEstablishSerialConnection(Exception):
+    pass
+
 class USBController(Service):
+    retries = 6
+    interval = 3
     def __init__(self, device, baudrate):
         self.device = device
         self.baudrate = baudrate
         self.port = None
+        self.attempt = None
     @property
     def protocol(self):
         return self.port.protocol if self.port else None
     def startConnection(self):
-        log.msg('opening %s at %s' % (self.device, self.baudrate))
-        self.port = SerialPort(pbLuaSerialProtocol(self), self.device, reactor, baudrate=self.baudrate)
+        self.attempt = 0
+        reactor.callWhenRunning(self._startConnection)
+    def _startConnection(self):
+        self.attempt += 1
+        if self.attempt > self.retries:
+            log.msg('giving up; USBController remains stopped')
+            self.stopService()
+            return
+        try:
+            self.port = SerialPort(pbLuaSerialProtocol(self), self.device, reactor, baudrate=self.baudrate)
+            log.msg('opened %s at %s' % (self.device, self.baudrate))
+        except SerialException, error:
+            log.msg('unable to open %s (%d/%d retries)' % (os.path.basename(self.device), self.attempt, self.retries))
+            reactor.callLater(self.interval, self._startConnection)
     def tearConnection(self):
         log.msg('closing %s' % (self.device))
         self.protocol.transport.loseConnection()
@@ -28,6 +49,7 @@ class USBController(Service):
         self.startConnection()
     def stopService(self):
         Service.stopService(self)
-        self.tearConnection()
+        if self.port is not None:
+            self.tearConnection()
     def setState(self, state, *args, **kwargs):
         self.protocol.setState(state, *args, **kwargs)
