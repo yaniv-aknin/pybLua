3c4fd2faafae6e95d951478067b1e6326a1d4021
Yaniv Aknin <yaniv@aknin.name>
2009-08-30 09:23:51 +0300

    move non blocking socket handler to yalib

--- pc/log.py
+++ pc/log.py
@@ -1,33 +1,8 @@
-import logging.handlers
 import logging
-import socket
 
 from twisted.python import log
 
-class NonBlockingDatagramHandler(logging.handlers.DatagramHandler):
-    "Mostly the vanilla DatagramHandler with some extra safety/non-blockingess additions"
-    def __init__(self, host='127.0.0.1', port=logging.handlers.DEFAULT_UDP_LOGGING_PORT):
-        logging.handlers.DatagramHandler.__init__(self, host, port)
-
-    def makeSocket(self):
-        "The DatagramHandler factory is overriden to create a non-blocking socket."
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setblocking(False)
-        return sock
-
-    def send(self, s):
-        """
-        Add exception handling. I've read the CPython code of sendto and am not sure it can create an exception
-         on a nonblocking datagram socket, but better safe than sorry.
-        """
-        if self.sock is None:
-            self.createSocket()
-        try:
-            self.sock.sendto(s, (self.host, self.port))
-        except socket.error:
-            self.sock.close()
-            # Note: so we can call createSocket next time
-            self.sock = None
+from yalib.logutils import NonBlockingDatagramHandler
 
 def setupLogging(logFile=None):
     if logFile is None:
